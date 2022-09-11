1ca6db38a1f657adab65aa6119a7330e261be8fe
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2022-09-11 13:05:37 +0300

    some edits

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Runlist
+
+Runs your exe files and processes in a order
+
+## Usage
+
+- It will get documented well soon. For now, you can look at the source code
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Operator } from "./run-operator";
 
 const fs = require("fs");
 
-const proccessMapStr = fs.readFileSync("sprintlist.json");
+const proccessMapStr = fs.readFileSync("runlist.json");
 const obj = JSON.parse(proccessMapStr);
 const operator = new Operator();
 
