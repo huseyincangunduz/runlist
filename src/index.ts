ddaeb5db9051af08be8190c88b73dfe4dd37c06d
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2022-09-11 22:52:20 +0300

    custom json and defaultly reads same named json with exe

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import path from "path";
 import { Operator } from "./run-operator";
+import fs from "fs";
+let filename: string;
+console.info("WORKING ON " + process.cwd());
+const customArg = process.argv.findIndex((a) => a.includes("-c"));
+if (customArg > -1) {
+  filename = process.argv[customArg + 1];
+} else {
+  const pathArray = process.argv0.split(/[\\\\/]/g);
+  let exeName = pathArray[pathArray.length - 1];
+  exeName = exeName.slice(0, exeName.lastIndexOf("."));
+  filename = path.resolve(process.cwd(), exeName + ".json");
+}
 
-const fs = require("fs");
-
-const proccessMapStr = fs.readFileSync("runlist.json");
+const proccessMapStr = fs.readFileSync(filename) as any as string;
 const obj = JSON.parse(proccessMapStr);
 const operator = new Operator();
 
--- src/run-operator.ts
+++ src/run-operator.ts
@@ -1,5 +1,6 @@
 import { RunModel } from "./run-model";
 import * as ChildProcess from "child_process";
+import path from "path";
 
 export const OperatorStatus = "";
 export class Operator {
@@ -7,37 +8,63 @@ export class Operator {
 
   operate(runModels: RunModel[]) {
     runModels.forEach((model) => {
-      const command = model.cmd[0],
-        params = model.cmd.length > 2 ? model.cmd.slice(1) : [];
-
-      let proccess;
       if (model.runMode == "continue-after-start") {
-        proccess = ChildProcess.execFile(command, params);
-        model._process = proccess;
-        this.continue(model.startAfterThis);
+        this.launchAndContinue(model);
       } else {
         if (model.waitBeforeExit) {
           this.waitFor.push(model);
         }
 
-        proccess = ChildProcess.execFile(
-          command,
-          params,
-          {},
-          (exception, output, processFinishError) => {
-            if (exception) console.error(exception);
-            if (processFinishError) console.error(processFinishError);
-            const modelIndex = this.waitFor.indexOf(model);
-            if (modelIndex > -1) this.waitFor.splice(modelIndex, 1);
-            this.continue(model.startAfterThis);
-          }
-        );
-        model._process = proccess;
+        this.launchWait(model);
       }
     });
   }
 
-  continue(startAfterThis: RunModel[]) {
+  private continue(startAfterThis: RunModel[]) {
     if (startAfterThis?.length) this.operate(startAfterThis);
+  }
+
+  private flatParameters(model: RunModel) {
+    const workingPath = process.cwd();
+    let args = model.cmd.length > 1 ? model.cmd.slice(1) : [];
+    args = args.map((str) => {
+      // if (
+      //   str.startsWith("./") ||
+      //   str.startsWith(".\\") ||
+      //   str.startsWith("../") ||
+      //   str.startsWith("..\\")
+      // ) {
+      //   console.info("PARAMETER : " + path.resolve(workingPath, str));
+      //   return path.resolve(workingPath, str);
+      // } else if (str.startsWith("/") || str.startsWith("\\")) {
+      //   return path.resolve(str);
+      // }
+      // console.info("PARAMETER : " + str);
+
+      return str;
+    });
+    return args;
+  }
+
+  private launchAndContinue(model: RunModel) {
+    const command = model.cmd[0],
+      params = this.flatParameters(model);
+    ChildProcess.spawn(command, params);
+    this.continue(model.startAfterThis);
+  }
+
+  private launchWait(model: RunModel) {
+    const command = model.cmd[0],
+      params = this.flatParameters(model);
+    const proccess = ChildProcess.spawnSync(command, params, {});
+    model._process = proccess;
+    const exception = proccess.error,
+      processFinishError = proccess.stderr;
+
+    if (exception) console.error(exception);
+    if (processFinishError) console.error(processFinishError);
+    const modelIndex = this.waitFor.indexOf(model);
+    if (modelIndex > -1) this.waitFor.splice(modelIndex, 1);
+    this.continue(model.startAfterThis);
   }
 }
